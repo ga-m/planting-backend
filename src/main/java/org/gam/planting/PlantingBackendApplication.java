6cca5527f5dca7336e711e331e9663414177f1bc
happygram <jsacc0619@gmail.com>
2020-01-25 14:50:41 +0900

    Initialize project

---
+++ src/main/java/org/gam/planting/PlantingBackendApplication.java
@@ -0,0 +1,13 @@
+package org.gam.planting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantingBackendApplication.class, args);
+	}
+
+}
