ce5332ab1abcd7281ec5802eb3954fd4a3ef271f
happygram <jsacc0619@gmail.com>
2020-03-01 22:55:41 +0900

    h2 데이터베이스 스키마 및 Mock 데이터 생성

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,8 @@
-INSERT INTO billionaires (first_name, last_name, career) VALUES
-  ('Aliko', 'Dangote', 'Billionaire Industrialist'),
-  ('Bill', 'Gates', 'Billionaire Tech Entrepreneur'),
-  ('Folrunsho', 'Alakija', 'Billionaire Oil Magnate');
+INSERT INTO `goal` (`id`, `member_id`, `name`, `description`, `status`, `type`, `order`, `create_timestamp`, `update_timestamp`) 
+VALUES (1, 1, 'test', 'test', 'active', 'basic', 1, '2020-01-01 00:00:00', '2020-01-01 00:00:00');
+
+INSERT INTO `level_basic` (`goal_id`, `seq`, `member_id`, `label`, `value`) 
+VALUES (1, 1, 1, 'test', 1);
+
+INSERT INTO `archive` (`id`, `goal_id`, `level_seq`, `member_id`, `create_timestamp`, `update_timestamp`) 
+VALUES (1, 1, 1, 1, '2020-01-01 00:00:00', '2020-01-01 00:00:00');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,31 @@
-DROP TABLE IF EXISTS billionaires;
- 
-CREATE TABLE billionaires (
-  id INT AUTO_INCREMENT  PRIMARY KEY,
-  first_name VARCHAR(250) NOT NULL,
-  last_name VARCHAR(250) NOT NULL,
-  career VARCHAR(250) DEFAULT NULL
+CREATE TABLE `goal` (
+  `id` BIGINT NOT NULL COMMENT '			',
+  `member_id` INT NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(45) NULL,
+  `status` ENUM('active', 'deleted') NOT NULL DEFAULT 'active',
+  `type` ENUM('basic', 'time', 'free') NOT NULL DEFAULT 'basic',
+  `order` SMALLINT NOT NULL,
+  `create_timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `level_basic` (
+  `goal_id` BIGINT NOT NULL,
+  `seq` BIGINT NOT NULL COMMENT '	',
+  `member_id` BIGINT NOT NULL,
+  `label` VARCHAR(45) NOT NULL,
+  `value` SMALLINT NOT NULL,
+  PRIMARY KEY (`goal_id`, `seq`)
+);
+
+CREATE TABLE `archive` (
+  `id` BIGINT NOT NULL,
+  `goal_id` BIGINT NOT NULL,
+  `level_seq` BIGINT NOT NULL,
+  `member_id` INT NOT NULL,
+  `create_timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
 );
