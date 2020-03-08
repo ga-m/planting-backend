e19149093918ec6007fec24159031be009e8811e
happygram <jsacc0619@gmail.com>
2020-03-08 22:42:37 +0900

    조회 메소드 생성

--- src/main/java/org/gam/planting/jpa/repository/LevelBasicRepository.java
+++ src/main/java/org/gam/planting/jpa/repository/LevelBasicRepository.java
@@ -4,4 +4,7 @@ import org.gam.planting.jpa.entity.LevelBasic;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LevelBasicRepository extends JpaRepository<LevelBasic, Long> {
+
+    LevelBasic findByGoalIdAndSeq(Long goalId, Long seq);
+    
 }
